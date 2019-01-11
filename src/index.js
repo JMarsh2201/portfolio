2c3d9def69fed255eba7ff0d82cc9b5abfdfe84e
Jake Marsh <JMarsh2201@gmail.com>
2019-01-11 10:32:38 -0700

    slideshow and header in place

--- src/index.js
+++ src/index.js
@@ -1,7 +1,41 @@
 import './style.css'
 
-const henlo = () => {
-  console.log("henlo")
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides() {
+  if (this.id === 'next') {
+    showSlides(slideIndex += 1);
+  } else {
+    showSlides(slideIndex += -1);
+  }
 }
 
-henlo()
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let cards = document.getElementsByClassName("card");
+  // let dots = document.getElementsByClassName("dot");
+  if (n > cards.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = cards.length}
+  for (i = 0; i < cards.length; i++) {
+      cards[i].style.display = "none";
+  }
+  // for (i = 0; i < dots.length; i++) {
+  //     dots[i].className = dots[i].className.replace(" active", "");
+  // }
+  cards[slideIndex-1].style.display = "block";
+  // dots[slideIndex-1].className += " active";
+}
+
+let next = document.getElementById('next')
+let prev = document.getElementById('prev')
+console.log(next, prev)
+
+next.addEventListener("click", plusSlides)
+prev.addEventListener("click", plusSlides)
